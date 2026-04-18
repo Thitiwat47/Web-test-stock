4f9b939bb099085ef1824b705aae42f994cc4dc3
Thitiwat2547 <ice351916@gmail.com>
2026-04-18 14:23:42 +0700

    f

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const jsdom = require("jsdom")
 
 
 const stock = [{
@@ -11,7 +10,7 @@ const stock = [{
 }];
 
 
-const listContainer = document.getElementById('stock-list');
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
