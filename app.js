f2d97cd555df9e03507878cc5eebdb01bbcd8b22
Thitiwat2547 <ice351916@gmail.com>
2026-04-18 15:38:25 +0700

    f

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const mysql = require("mysql2")
-
+const swaggerUi = require("swagger-ui-express")
+const YAML = require("yaml")
+const fs = require("fs")
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -12,6 +14,7 @@ const connection = mysql.createConnection({
 
 })
 
+const swaggerDocument = YAML.parse(fs.readFileSync(path.join(__dirname, "swagger.yaml"), "utf8"));
 
 
 
@@ -30,6 +33,19 @@ app.get("/data", (req, res) => {
     });
 });
 
+app.post("/update", (req, res) => {
+    const { name, price } = req.query;
+    connection.query("INSERT INTO stock (name, price) VALUES (?, ?) ON DUPLICATE KEY UPDATE price = ?", [name, price, price], (err, results) => {
+        if (err) {
+            console.error("Error updating data in database:", err);
+            res.status(500).json({ error: "Error updating data in database" });
+        } else {
+            res.json({ message: "Stock updated successfully" });
+        }
+    });
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
